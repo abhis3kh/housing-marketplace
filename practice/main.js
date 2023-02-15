385b608ba8aa41c4562776d1dae10758e140aaed
Abhisekh <abhisekhmukherjee101@gmail.com>
2023-02-15 16:11:47 +0530

    Deleted main.js file

--- practice/main.js
+++
@@ -1,8 +0,0 @@
-// const traditionalFunction = (name) => `Hello Developer, ${name} !!!`;
-
-// console.log(traditionalFunction('Abhisekh'));
-const spreadFuntion = (...args) =>
-  'You have passed ' + args.length + ' arguments.';
-
-console.log(spreadFuntion(0, 1, 2));
-console.log(spreadFuntion('string', null, [1, 2, 3], {}));
